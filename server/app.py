bb5de5fa3782ddc9f5ec95567968a5c04265804c
Felix-Okeyo <felixokeyo20@gmail.com>
2023-09-21 21:57:00 +0300

    done

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 
 from models import db, Message
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,13 +15,97 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        all_messages = Message.query.order_by(Message.created_at).all()
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+        all_messages_serialized = [message.to_dict()
+                                   for message in all_messages]
+
+        response = make_response(
+            jsonify(all_messages_serialized),
+            200
+        )
+
+        return response
+
+    if request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_serialized = new_message.to_dict()
+
+        response_body = {
+            "creation_successful": True,
+            "body": new_message_serialized['body'],
+            "username": new_message_serialized['username']
+        }
+
+        response = make_response(
+            jsonify(response_body),
+            201
+        )
+
+        return response
+
+    return None
+@app.route('/messages/<int:num>', methods=['PATCH', 'DELETE', 'GET'])
+def messages_by_id(num):
+
+    message = Message.query.filter_by(id=num).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_serialized = message.to_dict()
+
+        response = make_response(
+            jsonify(message_serialized),
+            200
+        )
+
+        return response
+
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+
+        return response
+
+    if request.method == 'GET':
+
+        message_serialized = message.to_dict()
+
+        response = make_response(
+            jsonify(message_serialized),
+            200
+        )
+
+        return response
+
+    return None
 
 if __name__ == '__main__':
     app.run(port=5555)
